b2334c9b169e196ab5023e7321eb3be3d05f5bff
divy-koushik <dk@adizen.ai>
2024-07-10 02:02:00 +0530

    minor bugs

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -13,14 +13,14 @@ const Card: React.FC<CardProps> = (props) => {
     <div className="flex flex-col items-center justify-center border rounded-3xl md:w-1/3 mx-4 overflow-hidden relative shadow-2xl hover:scale-110 transition">
       <img
         src={props.image || `https://cdn.pixabay.com/photo/2015/01/20/12/51/ipad-605420_640.jpg`}
-        className="min-h-72"
+        className="min-h-72 h-full"
       />
       <div className="w-full rounded-xl flex items-center justify-between bottom-4 absolute px-5">
         <div className="flex flex-col items-center rounded-full border w-full bg-white">
           <div className="flex w-full px-4 items-center justify-between">
-            <div className="text-center">
+            <div className="text-center py-4 flex justify-center">
               <h3 className="">{props.heading}</h3>
-              <h1 className="text-lg">{props.body}</h1>
+              <h1 className="text-lg text-center">{props.body}</h1>
             </div>
             <div className="border w-10 h-10 flex justify-center items-center border-black rounded-full">
               <RiArrowRightUpLine />
